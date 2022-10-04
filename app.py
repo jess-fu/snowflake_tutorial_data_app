db64e37b1d75cb67540aace80f2f9c057a495ed7
jessfu <jessfu@deloitte.co.nz>
2022-10-04 22:25:18 +1300

    change text

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit
 
-streamlit.title("Hello World!")
+streamlit.title("Healthy Dinner!")
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueburry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
+streamlit.text("🥑🍞Avocado Toast")
