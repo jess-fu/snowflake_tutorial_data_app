0c074f05dff21428d57881fa9c5df130cd2e22e5
jessfu <jessfu@deloitte.co.nz>
2022-10-04 22:43:48 +1300

    add selection logic

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ my_fruit_list = pd.read_csv(
     "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
 )
 my_fruit_list = my_fruit_list.set_index("Fruit")
-# Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(my_fruit_list)
+# Let's put a pick list here so they can pick the fruit they want to include
+streamlit.dataframe(fruits_to_show)
