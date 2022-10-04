4b72cb2c85c007c4c45ac6e7de8b6ab08e13b56f
jessfu <jessfu@deloitte.co.nz>
2022-10-04 22:20:51 +1300

    add some text

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+from isort import stream
 import streamlit
 
 streamlit.title("Hello World!")
 streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueburry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
